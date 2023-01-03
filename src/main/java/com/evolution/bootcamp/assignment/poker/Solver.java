e5edb7f86b700a5022e502def9cb06ab39d11855
sbx_user1051 <sbx_user1051@169.254.218.213>
2023-01-03 17:39:59 +0000

    Initial commit

---
+++ src/main/java/com/evolution/bootcamp/assignment/poker/Solver.java
@@ -0,0 +1,10 @@
+package com.evolution.bootcamp.assignment.poker;
+
+class Solver {
+
+  // TODO: implement correct solution logic
+  static String process(String line) {
+    throw new UnsupportedOperationException("Not implemented yet...");
+  }
+
+}
